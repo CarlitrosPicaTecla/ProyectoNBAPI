bb943acddf8cc29be43842a860880abc6d9db8c2
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-10-20 20:54:53 +0200

    A

--- NBAPI/src/app/app.module.ts
+++ NBAPI/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialsImportModule } from './materials-import/materials-import.module';
+import { TeamListComponent } from './components/team-list/team-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamListComponent
   ],
   imports: [
     BrowserModule,
