562479731c50aa10d39fc62107fe3bb9e4cc6cf0
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-10-20 21:44:25 +0200

    .

--- NBAPI/src/app/app.module.ts
+++ NBAPI/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialsImportModule } from './materials-import/materials-import.module';
 import { TeamListComponent } from './components/team-list/team-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LandingComponent } from './components/landing/landing.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamListComponent
+    TeamListComponent,
     LandingComponent,
     NotFoundComponent
   ],
