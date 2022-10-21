661cfbbee1c00e0822a721f9965dfe5d057b9788
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-10-21 12:33:11 +0200

    en progreso

--- NBAPI/src/app/components/player-list/player-list.component.ts
+++ NBAPI/src/app/components/player-list/player-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Player } from 'src/app/interfaces/player.interface';
 import { PlayerService } from 'src/app/services/player.service';
 
@@ -11,15 +14,22 @@ export class PlayerListComponent implements OnInit {
 
   playerList: Player[] = [];
   displayedColumns: string[] = ['nombre', 'equipo', 'posicion', 'altura', 'peso', 'pais'];
+  datos = new MatTableDataSource();
   
+  @ViewChild(MatPaginator) paginator: MatPaginator = {} as MatPaginator;
+  @ViewChild(MatSort) sort: MatSort = {} as MatSort;
   
   
   constructor(private playerService: PlayerService) { }
   
   ngOnInit(): void {
     this.playerService.getPlayers(2022).subscribe(respuesta => {
-      this.playerList = [...respuesta.league.standard, ...respuesta.league.africa, ...respuesta.league.sacramento, ...respuesta.league.utah, ...respuesta.league.vegas];      
+      this.playerList = [...respuesta.league.standard, ...respuesta.league.africa, ...respuesta.league.sacramento, ...respuesta.league.utah, ...respuesta.league.vegas];
+      this.datos.data = this.playerList;      
+      this.datos.paginator = this.paginator;
+      this.datos.sort = this.sort;
     });
+    console.log(this.datos)
   }
 
 }
