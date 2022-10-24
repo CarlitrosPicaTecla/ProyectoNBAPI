e3e63839ecb2cf5e81fed1d9d76b76c426f75bae
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-10-24 19:50:11 +0200

    trabajo casa

--- NBAPI/src/app/components/team-info/team-info.component.ts
+++ NBAPI/src/app/components/team-info/team-info.component.ts
@@ -25,12 +25,17 @@ export class TeamInfoComponent implements OnInit {
 
   playerList : Player[]= [];
 
+  currentYear = 2022;
+
+  years : number[] = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+
   constructor(private route: ActivatedRoute, private teamService : TeamService, private playerservice : PlayerService) { }
 
   ngOnInit(): void {
     this.getParams();
     this.getTeamInfo(this.yearLink);
     this.getPlayersOfTeam(this.yearLink);
+    this.showImg(this.idLink);
   }
 
   getTeamInfo(year:number){
@@ -65,11 +70,32 @@ getPlayersOfTeam(year : number){
         this.teamPlayerList.push(player);
       }
     }
-
-
   });
 
 }
 
+showImg(idLogo : string){
+  
+    return `https://cdn.nba.com/logos/nba/${idLogo}/global/L/logo.svg`;
+}
+getPlayerImg(jugador: Player) {
+  return `https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/${jugador.personId}.png`
+}
+
+choseSelect(year: number) {
+  this.currentYear = year;
+  this.cargarJugadores(year);
+}
+
+cargarJugadores(year: number) {
+  this.playerservice.getPlayers(year).subscribe(respuesta => {
+    this.playerList = respuesta.league.standard;
+    this.teamService.getTeam(year).subscribe(respuesta => {
+      this.teamList = respuesta.league.standard;
+    })
+    
+  });
+
+}
 
 }
