ec636e6a60599e88e6a310d04fc0c09ecd677cce
Carlos Jesús Durbán Viloca <carlosdurban91@gmail.com>
2022-10-26 14:21:40 +0200

    standings terminado

--- NBAPI/src/app/components/standings/standings.component.ts
+++ NBAPI/src/app/components/standings/standings.component.ts
@@ -1,8 +1,11 @@
 import { ThisReceiver } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Team } from 'src/app/interfaces/standings.interface';
 import { StandingsService } from 'src/app/services/standings.service';
+import { MatTable } from '@angular/material/table';
+import { MatTab } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-standings',
@@ -12,19 +15,29 @@ import { StandingsService } from 'src/app/services/standings.service';
 export class StandingsComponent implements OnInit {
 
   rankingTeams: Team[] = [];
+  @ViewChild(MatSort) orden!: MatSort;
   datos = new MatTableDataSource<Team>();
   displayedColumns: string[] = ['ranking', 'nombre', 'victorias', 'derrotas', 'porcentajeVD', 'balanceConf',
-   'balanceLocal', 'balanceVisitante', 'lastTen', 'racha', 'mediaPuntosATK', 'mediaPuntosDEF', 'diferenciaPTS'];
+   'balanceLocal', 'balanceVisitante', 'lastTen', 'racha'];
+  
+  
+   currentYear = 2022;
+
 
   constructor(private standingsService: StandingsService) { }
 
   ngOnInit(): void {
     this.cargarStandings();
+    
   }
-
+  
+  
+  
   cargarStandings() {
     this.standingsService.getAllStandings().subscribe(respuesta => {
       this.rankingTeams = respuesta.league.vegas.teams;
+      this.datos = new MatTableDataSource(this.rankingTeams);
+      this.datos.sort = this.orden;
     });
   }
 
